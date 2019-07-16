d87ccdc61d7a106a8b7545d4b795d5b785699f1c
Joseph Estabrook <JEstabrook@users.noreply.github.com>
2019-07-16 10:51:37 -0700

    updating sample subsetting procedure

--- scripts/run_glimma.R
+++ scripts/run_glimma.R
@@ -1,11 +1,16 @@
 library(Glimma)
 library(limma)
 library(DESeq2)
+library(stringr)
 
 condition = snakemake@params[['condition']]
 cat(sprintf(c('Condition: ',condition,'\n')))
 
-title = snakemake@params[["contrast"]]
+ma_plot_ = snakemake@output[['ma_plot']]
+volcano_plot_ = snakemake@output[['volcano_plot']]
+ma_plot = str_sub(tail(strsplit(ma_plot_,'/')[[1]],n=1),1,-6)
+volcano_plot = str_sub(tail(strsplit(volcano_plot_,'/')[[1]],n=1),1,-6)
+
 
 contrast = c(condition, snakemake@params[["contrast"]])
 rds = snakemake@input[['rds']]
@@ -19,7 +24,6 @@ groups.df = as.data.frame(colData(rds))
 
 
 #### by contrasts
-contrasts_to_plot = resultsNames(rds)
 res <- results(rds, contrast=contrast)
 res$padj[is.na(res$padj)] = 1
 
@@ -33,13 +37,13 @@ status_frame$padj[is.na(status_frame$padj)] = 1
 status_frame[status_frame$padj<0.05 & status_frame$log2FoldChange < 0 ,'status'] = -1
 status_frame[status_frame$padj<0.05 & status_frame$log2FoldChange > 0 ,'status'] = 1
 
-title = paste(title[1],'vs',title[2],sep='-')
-
 glMDPlot(res, anno=genes, status=status_frame$status, samples=colnames(rnaseq), 
-         counts=log2(rnaseq + 0.0001), groups=groups.df[[condition]], main=strsplit(res@elementMetadata$description[2],': ')[[1]][2], 
-         transform=F, side.ylab='Log2-expression',launch=FALSE,side.main='GeneID', html = paste(title,'ma_plot',sep='.'),path=out_path)
+         counts=log2(rnaseq + 0.0001),
+        groups=groups.df[[condition]], main=strsplit(res@elementMetadata$description[2],': ')[[1]][2],
+         transform=F, side.ylab='Log2-expression',launch=FALSE,side.main='GeneID', html = ma_plot, path=out_path)
 
 ## Volcano plot
 glXYPlot(x=res$log2FoldChange, y=-log10(res$pvalue), xlab="logFC", ylab="logodds",path=out_path,
-         status=status_frame$status, launch=FALSE,counts=log2(rnaseq + 0.0001), groups=groups.df[[condition]], anno=genes,main=strsplit(res@elementMetadata$description[2],': ')[[1]][2],html = paste(title,'volcano_plot',sep='.'))
+         status=status_frame$status, launch=FALSE,counts=log2(rnaseq + 0.0001), groups=groups.df[[condition]],
+         anno=genes, main=strsplit(res@elementMetadata$description[2],': ')[[1]][2], html = volcano_plot)
 
